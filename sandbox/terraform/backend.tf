2cdfdb22275862fb4e089cf3832849ebd3693bd6
Tony Sh <128180505+Anatolich911@users.noreply.github.com>
2024-08-20 10:35:26 -0400

    Create backend.tf

---
+++ sandbox/terraform/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 0.15.0"
+  backend "s3" {
+    region         = "us-east-2"
+    bucket         = "aft-backend-575108919022-primary-region"
+    key            = "account-provisioning-customizations/terraform.tfstate"
+    dynamodb_table = "aft-backend-575108919022"
+    encrypt        = "true"
+    kms_key_id     = "556bce9d-6c3b-4ac3-93bc-44ea8e15d0df"
+    role_arn       = "arn:aws:iam::575108919022:role/AWSAFTExecution"
+  }
+}
