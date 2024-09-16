981a2a60521fed254f420028f7be0cc0a885bc9d
Tony Sh <128180505+Anatolich911@users.noreply.github.com>
2024-09-16 15:17:25 -0400

    Update provider.tf

--- sandbox/terraform/provider.tf
+++ sandbox/terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-2"
   assume_role {
-    role_arn    = "arn:aws:iam::739153323366:role/AWSAFTExecutions"
+    role_arn    = "arn:aws:iam::739153323366:role/AFT"
   }
   default_tags {
     tags = {
